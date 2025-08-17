324489c2940f2da07b0f000907e22df9cf2767b5
Vinicius Fillos <vinifillos@gmail.com>
2025-08-17 18:25:07 -0300

    feature: added initial path configurations

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ server.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.sendFile('page.html', {root: __dirname});
 });
+
+server.get('/teste', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile('teste.html', {root: __dirname});
+});
+
+server.use((req, res) => {
+    res.status(404).sendFile('page_not_found.html', {root: __dirname});
+});
