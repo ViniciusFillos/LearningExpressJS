b02833fbe4dd2378b4e47c0094956b63cc9031d7
Vinicius Fillos <vinifillos@gmail.com>
2025-08-17 22:00:49 -0300

    feature: get method for users implemented

--- index.js
+++ index.js
@@ -4,15 +4,30 @@ const server = express();
 
 server.listen(3000);
 
+const users = [{
+    id: 0,
+    name: 'Vinicius',
+    age: 21
+}];
+
 server.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.sendFile('page.html', {root: __dirname});
 });
 
-server.get('/teste', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile('teste.html', {root: __dirname});
-});
+server.get('/users', (req, res) => {
+    res.status(200).json(users);
+})
+
+server.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find(x => x.id == id);
+
+    if(user)
+        res.status(201).send(user);
+    else 
+        res.status(404).send('User not found');
+})
 
 server.use((req, res) => {
     res.status(404).sendFile('page_not_found.html', {root: __dirname});
